69cb9719209a7b6be78bfde95d5af39c3c1a2bcb
billow <yaagnto@qq.com>
2019-03-02 17:41:03 +0800

    get data: 1

--- src/components/InfoItem.vue
+++ src/components/InfoItem.vue
@@ -1,7 +1,14 @@
 <template>
-  <section>
-    {{category}}
-  </section>
+  <el-table v-if="!items.length>0"
+            :data="items.data._embedded.posts"
+            style="width: 100%">
+    <el-table-column
+      prop="title">
+    </el-table-column>
+    <el-table-column
+      prop="createdAt">
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
@@ -14,6 +21,9 @@
       return {
         items: [],
       }
+    },
+    async mounted() {
+      this.items = await this.$api.get('posts')
     },
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
+const api = axios.create(
+  {
+    baseURL: 'http://localhost:8081/api',
+  },
+)
+Vue.use({
+  install(Vue) {
+    Vue.prototype.$api = api
+  },
+})
+
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
