a8de302b06ddda5d9e8d2cd72c1a5601c7c7ec11
billow <yaagnto@qq.com>
2019-03-09 15:03:13 +0800

    user repo

--- server/src/main/kotlin/billow/fun/cms/server/model/User.kt
+++ server/src/main/kotlin/billow/fun/cms/server/model/User.kt
@@ -1,4 +1,11 @@
 package billow.`fun`.cms.server.model
 
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.query.Param
+
 data class User(val username: String,
                 val password: String)
+
+interface UserRepo : JpaRepository<User, Long> {
+    fun findByUsername(@Param("username") username: String): User?
+}
